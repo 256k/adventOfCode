3dacbd5fff7620833604fb70c71acc2f398fbb51
fayek.helmi <fayek.helmi@gmail.com>
2021-12-01 22:21:10 -0500

    added the solution to day1

---
+++ 2021/day01/day1.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+
+const compare = (array) => {
+	let count = 0;
+	let prevNum = 0;
+	array.forEach((num,index) => {
+		num > prevNum ? count+= 1: null;
+		prevNum = num; 
+	})
+console.log(count);
+};
+
+
+const fileContentRaw = fs.readFileSync('./input.txt', 'utf-8');
+const fileContent = fileContentRaw.split("\n");
+
+
+const windowValues = [];
+fileContent.forEach((number, index) => { 
+	windowValues.push( parseInt(number )+ parseInt(fileContent[index + 1]) +parseInt( fileContent[index+2]) - 1 );
+});
+
+compare(fileContent);
+compare(windowValues);
+
+
+
+/*
+var file = fs.createWriteStream('array.txt');
+fileContent.forEach(function(v) { file.write(v + '\n'); });
+file.end();
+*/
+
---
+++ README.md
@@ -0,0 +1,1 @@
+my solutions to the Advent of Code yearly challenges found here: https://adventofcode.com/
